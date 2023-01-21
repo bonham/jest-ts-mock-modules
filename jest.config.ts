1f646d87944d3c7bdbc4636442b8f66ba298f771
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-01-21 16:08:51 +0100

    baseline

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+
+};
+export default jestConfig
