725145bb590b4e4980bd14cb238cd04ec5840545
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-01-22 15:56:21 +0100

    cleanup config

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,6 @@ const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
     '^.+\\.[tj]sx?$': [
       'ts-jest',
       {
