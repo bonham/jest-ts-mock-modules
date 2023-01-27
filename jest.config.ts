2bd02cff0e8cf73b14b6136b9b9a4a471635a0a1
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-01-27 22:37:35 +0100

    make module resolution consistent
    for node

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,17 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests_javascript/**','**/tests_typescript/**'],
+  extensionsToTreatAsEsm: [".ts"],
+  roots: [
+    './tests_javascript',
+    './tests_typescript'
+  ],
+  testMatch: [
+    '**/*.(js|ts)'
+  ],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
     '^.+\\.[tj]sx?$': [
       'ts-jest',
